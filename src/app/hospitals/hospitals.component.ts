d0598f43b3a30ed75f7e7272b8de8014e01cab6b
AJay Marathe <0107amar@gmail.com>
2019-06-03 10:09:50 +0530

    hospitial functionality done

--- src/app/hospitals/hospitals.component.ts
+++ src/app/hospitals/hospitals.component.ts
@@ -13,52 +13,64 @@ export class HospitalsComponent implements OnInit {
   address;
   doctor;
   patient;
+  isActive = false;
+  selectedItem;
+
 
   hospital = [
     {
       name : 'first hospital',
       address: 'pune',
       doctor: 'vikas',
-      patient: 'vimal'
+      patient: 'vimal',
+      isSelected:true
     },
     {
       name :  'second hospital',
       address: 'pune',
       doctor: 'fdfdsfd',
-      patient: 'vimal'
+      patient: 'vimal',
+      isSelected:false
     },
     {
       name : 'third hospital',
       address: 'pune',
       doctor: 'hello',
-      patient: 'vimal'
+      patient: 'vimal',
+      isSelected:false
     },
     {
       name : 'fourth hospital',
       address: 'pune',
       doctor: 'dd dsdlfsdfsdfsdf',
-      patient: 'vimal'
+      patient: 'vimal',
+      isSelected:false
     },
 
   ]
 
   ngOnInit() {
-    console.log(this.hospital);
+    this.FirstShouldbeSelected();
   }
 
+  listClick(event, newValue) {
+    this.selectedItem = newValue;  // don't forget to update the model here
+}
+
+  FirstShouldbeSelected(){
+    const hospti_data = [this.name = this.hospital[0].name, this.address =  this.hospital[0].address, this.doctor =  this.hospital[0].doctor, this.patient=  this.hospital[0].patient];
+  }
+
+
   showData(name){
-    // console.log(name)
-    
-
-
     var i;
     for (i = 0; i < this.hospital.length; i++) {
       if(name == this.hospital[i].name){
-        console.log(this.hospital[i].name)
         const hospti_data = [this.name = this.hospital[i].name, this.address =  this.hospital[i].address, this.doctor =  this.hospital[i].doctor, this.patient=  this.hospital[i].patient];
-        console.log(hospti_data);
+        this.hospital[i].isSelected=true;
+      }else{
+        this.hospital[i].isSelected=false
       }
     }
-
   }
 }
